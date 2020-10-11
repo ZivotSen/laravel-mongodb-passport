80d1b57443f28f225150cef00372aadb8e50e154
ZivotSen <aogvarela@gmail.com>
2020-10-10 23:28:07 -0400

    Adding support for Auth User extending from correct mongodb model

---
+++ src/Auth/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Zivot\Mongodb\Passport\Auth;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Jenssegers\Mongodb\Eloquent\Model;
+use Zivot\Mongodb\Passport\HasApiTokens;
+
+class User extends Model implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail, HasApiTokens;
+}
